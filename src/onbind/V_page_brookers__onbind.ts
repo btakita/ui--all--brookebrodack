548475524592a316aa881b62c7e42cc59e20c6b6
Brian Takita <brian.takita@gmail.com>
2023-11-08 20:00:21 -0500

    changes:
    	+ V_page_brookers__onbind
    	+ V_page_brookers__onbind_
    
    package.json:
    
    	fix: exports: + types: src/index.ts
    	∋ @ctx-core/dom
    	∋ @ctx-core/object

---
+++ src/onbind/V_page_brookers__onbind.ts
@@ -0,0 +1,10 @@
+/// <reference lib="dom" />
+import { id__dom__handler_ } from '@ctx-core/dom'
+import { be_, type Ctx } from '@ctx-core/object'
+export const V_page_brookers__onbind_ = be_<
+	((V_page_home:HTMLElement, ctx:Ctx)=>void)|null
+>('V_page_brookers__onbind_', ()=>null)
+export const V_page_brookers__onbind = id__dom__handler_(
+	'V_page_brookers__onbind',
+	(V_page_home:HTMLDivElement, ctx:Ctx)=>
+		V_page_brookers__onbind_(ctx)!(V_page_home, ctx))
