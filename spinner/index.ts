568acfc395ef90c5da113c2d50152d205851722e
Brian Takita <brian.takita@gmail.com>
2024-02-13 22:04:46 -0500

    + ./spinner: + spinner__template_
    update dependencies

--- spinner/index.ts
+++ spinner/index.ts
@@ -1,1 +1,2 @@
 export * from './spinner__div.js'
+export * from './spinner__template.js'
---
+++ spinner/spinner__template.ts
@@ -0,0 +1,28 @@
+import { class_ } from 'ctx-core/html'
+import { type relement_env_T } from 'relementjs'
+import { template_ } from 'relementjs/html'
+import { spinner__div_ } from './spinner__div.js'
+export function spinner__template_<env_T extends relement_env_T>({
+	class:_class,
+	spinner_class,
+}:{
+	class?:string
+	spinner_class?:string
+}) {
+	return (
+		template_<env_T>({
+			id: 'spinner__template',
+			class: class_(
+				'hidden',
+				_class),
+			/** @see {import('@btakita/ui--browser--brookebrodack/spinner').spinner__template__hyop} */
+			hyop: 'spinner__template__hyop'
+		}, [
+			spinner__div_({
+				class: class_(
+					'spinner',
+					spinner_class)
+			})
+		])
+	)
+}
